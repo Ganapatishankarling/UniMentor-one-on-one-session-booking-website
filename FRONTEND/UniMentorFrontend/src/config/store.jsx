4fff2894f8971c2ace22ac96eda80f5a0988198d
Ganapatishankarling <ganapatishankarling66@gmail.com>
2025-05-04 11:23:23 +0530

    updated

--- FRONTEND/UniMentorFrontend/src/config/store.jsx
+++ FRONTEND/UniMentorFrontend/src/config/store.jsx
@@ -1,11 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from '../slices/userSlice'
 import accountReducer from '../slices/accountSlice'
+import categoryReducer from '../slices/categorySlice'
+import profileReducer from '../slices/userSlice'
 
 const store = configureStore({
     reducer:{
         users:userReducer,
-        account:accountReducer
+        account:accountReducer,
+        categories:categoryReducer,
+        profile:profileReducer
     }
 })
 export default store
