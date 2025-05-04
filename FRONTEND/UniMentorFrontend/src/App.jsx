75e656f5c56a40f3ed15c8e358bd49cc689a1399
Ganapatishankarling <ganapatishankarling66@gmail.com>
2025-05-04 11:28:21 +0530

    added routes

--- FRONTEND/UniMentorFrontend/src/App.jsx
+++ FRONTEND/UniMentorFrontend/src/App.jsx
@@ -5,9 +5,15 @@ import {useEffect} from 'react'
 import Register from './components/Register.jsx'
 import Login from './components/Login.jsx'
 import Account from './components/Account.jsx'
+import Profile from './components/Profile.jsx'
+import EditProfile from './components/EditProfile.jsx'
+import {fetchProfile} from './slices/userSlice.jsx'
 import {useSelector,useDispatch} from 'react-redux'
 import { fetchUserAccount } from './slices/accountSlice.jsx'
 import {login,logout} from './slices/userSlice.jsx'
+import Category from './components/Category.jsx'
+import CategoryForm from './components/CategoryForm.jsx'
+import Test from './components/test.jsx'
 
 export default function App(){
   const dispatch = useDispatch()
@@ -24,9 +30,10 @@ export default function App(){
     <div className='App'>
       <h1>Welcome to UniMentor</h1>
        <ul>
-        {isLoggedIn ?( 
+        {isLoggedIn === false ?( 
           <>
           <li><Link to='/account'>Account</Link></li>
+          <li><Link to='/category'>Category</Link></li>
           <li><button onClick={()=>{dispatch(logout());localStorage.removeItem('token');navigate('/login')}}>Logout</button></li>
         </>
         ):(
@@ -40,6 +47,11 @@ export default function App(){
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/account' element={<Account/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/edit-profile' element={<EditProfile/>}/>
+        <Route path='/category' element={<Category/>}/>
+        <Route path='/add-category/:id' element={<CategoryForm/>}/>
+        <Route path='/add-category' element={<CategoryForm/>}/>
       </Routes>
     </div>
   )
