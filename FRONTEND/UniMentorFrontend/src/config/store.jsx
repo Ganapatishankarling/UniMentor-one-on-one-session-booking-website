346da475a7bcf27fc48afa51b659621e236d2ba2
Ganapatishankarling <ganapatishankarling66@gmail.com>
2025-05-17 22:00:34 +0530

    updated store

--- FRONTEND/UniMentorFrontend/src/config/store.jsx
+++ FRONTEND/UniMentorFrontend/src/config/store.jsx
@@ -3,13 +3,15 @@ import userReducer from '../slices/userSlice'
 import accountReducer from '../slices/accountSlice'
 import categoryReducer from '../slices/categorySlice'
 import profileReducer from '../slices/userSlice'
+import sessionReducer from '../slices/sessionSlice'
 
 const store = configureStore({
     reducer:{
         users:userReducer,
         account:accountReducer,
         categories:categoryReducer,
-        profile:profileReducer
+        profile:profileReducer,
+        sessions:sessionReducer
     }
 })
 export default store
