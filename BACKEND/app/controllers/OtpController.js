5c4dc0f2c02354ce29bfe5a7cb4655377a265d30
Ganapatishankarling <ganapatishankarling66@gmail.com>
2025-06-04 14:28:52 +0530

    controller for otp verification

--- BACKEND/app/controllers/OtpController.js
+++ BACKEND/app/controllers/OtpController.js
@@ -1,4 +1,3 @@
-
 import OTPModel from '../models/OtpModel.js';
 import UserModel from '../models/UserModel.js';
 
@@ -11,7 +10,7 @@ OTPController.sendOtp = async(req,res)=>{
         const otpDigit = Math.floor(100000 + Math.random() * 900000).toString();
         const user = await UserModel.findOne({email})
         if(user){
-            res.status(400).json({message:"User already exist"})
+           return res.status(400).json({message:"User already exist"})
         }
         const otpExpiryAt = new Date(Date.now() + 10 * 60 * 1000)
         const body = {
@@ -30,30 +29,34 @@ OTPController.sendOtp = async(req,res)=>{
     }
 }
 
-OTPController.verify = async(req,res)=>{
-    try {
-        const {email,otp} = req.body
-        const userOtp = await OTPModel.findOne({email})
-       
-        if (!otp) {
-             return res.status(400).json({ errors: 'No OTP found for this email' });
-        }
-        
-        if(userOtp.otp !== otp || userOtp.otpExpiryAt  < new Date()){
-        
-        await OTPModel.findOneAndDelete({email})
-        return res.status(400).json({errors:'OTP does not match or OTP expired'})
-        }
-        userOtp.isVerified = true
-        await userOtp.save()
-        
-        return res.status(200).json({message:'OTP verified successfully'})
-
-    } catch (error) {
-          console.log(error)
-          
-        return res.status(500).json({errors:error.message})
+OTPController.verify = async (req, res) => {
+  console.log('Verifying OTP...');
+  try {
+    const { email, otp } = req.body;
+    const userOtp = await OTPModel.findOne({ email });
+  
+    
+    if (!userOtp) {
+      return res.status(400).json({ errors: 'No Otp found please resend OTP' });
     }
-}
+    console.log('OTP',userOtp.otp== Number(otp))
 
+
+    if(userOtp.otp == otp){
+     userOtp.isVerified = true;
+     await userOtp.save();
+    // await OTPModel.findOneAndDelete({ email });
+
+    return res.status(200).json({ message: 'OTP verified successfully' });
+    }else{
+        await OTPModel.findOneAndDelete({ email })
+        return res.status(400).json({ errors: 'err 2 Invalid or expired OTP'});
+    }
+
+  } catch (error) {
+    console.error('OTP Verification Error:', error);
+     await OTPModel.findOneAndDelete({ email })
+    return res.status(500).json({ errors: error.message });
+  }
+};
 export default OTPController
